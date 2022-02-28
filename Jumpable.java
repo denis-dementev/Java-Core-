9ce19f39915b6425571691e1ed0285c4af82b380
Denis Dementev <100590072+denis-dementev@users.noreply.github.com>
2022-02-28 22:11:05 +0700

    Update Jumpable.java

--- Jumpable.java
+++ Jumpable.java
@@ -1,11 +1,6 @@
 
 package animals;
-/**
- * Write a description of interface Jumpable here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
+
 public interface Jumpable {
     boolean jump(float height);
     float getJumpLimit();
