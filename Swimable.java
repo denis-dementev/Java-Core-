10bcb7038dda4328c29fe62071c6231fedaeea17
Denis Dementev <100590072+denis-dementev@users.noreply.github.com>
2022-02-28 22:11:32 +0700

    Update Swimable.java

--- Swimable.java
+++ Swimable.java
@@ -1,11 +1,5 @@
-
 package animals;
-/**
- * Write a description of interface Swimable here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
+
 public interface Swimable {
     boolean swim(int length);
     int getSwimLimit();
