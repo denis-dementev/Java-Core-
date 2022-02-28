7e6a236f04d367c0a18971d9cef9af516245d2d5
Denis Dementev <100590072+denis-dementev@users.noreply.github.com>
2022-02-28 22:06:01 +0700

    Update Animal.java

--- Animal.java
+++ Animal.java
@@ -1,10 +1,5 @@
 package animals;
-/**
- * Abstract class Animal - write a description of the class here
- *
- * @author (your name here)
- * @version (version number or date here)
- */
+
 public abstract class Animal {
     protected String name;
     protected int run_limit;
